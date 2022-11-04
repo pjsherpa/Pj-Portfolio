99b69c03f89dc22b26268d8699f9533986a680d8
Phinjock Sherpa <phinjock@gmail.com>
2022-11-03 17:59:13 -0700

    updated portfolio with image updated hover feature for project WIP polishing

--- src/component/pages/AboutMe.js
+++ src/component/pages/AboutMe.js
@@ -1,23 +1,28 @@
 import React from "react";
-
+import Card from "react-bootstrap/Card";
+import Pjimg from "../../images/pj image.jpeg";
 const AboutMe = () => {
   return (
     <>
-      <div className="container">
-        <h1>About Me</h1>
+      <div className="container ">
+        <h1 className="d-flex justify-content-md-center ">About Me</h1>
+        <div className="d-flex justify-content-md-center ">
+          <div className="col-lg-2 mb-2">
+            <Card.Img src={Pjimg} />
+          </div>
+        </div>
         <p>
-          Growing a Passion in Coding. I am a front-end web developer holding a
-          Bachelor of Business in Accounting. I'm working my way to earn a
-          certificate in full stack development from UC Berkeley, where I
-          developed skills in JavaScript, HTML, CSS, responsive web design,
-          node, REST API,Progress web apps and React. As an innovative
-          problem-solver passionate about developing apps, my current focus is
-          on mobile-first design and development. I bring the following to every
-          project: •Problem-solving skills and perseverance •Dedication to
-          teamwork and flexible collaboration •Understanding of UX/UI principles
-          •I’m excited to leverage my skills as part of a fast-paced,
-          quality-driven team. Let’s connect!
+          My name is Phinjock Sherpa aka Pj Having the oppurtunity to work
+          alongside developers in my previous job as an end user. Inspired and
+          lead me to learn full stack web development from UC Berkeley. My aim
+          is to become a Front End Engineer. I had a huge role communicating
+          with management,clients,vendors and external auditors. I had a role
+          where I was teaching my colleagues on how to use a new cloud based
+          system designed specifically for the company.
         </p>
+        <p>I'm adventurous and love to travel. </p>
+
+        <p>Thank you for your time.</p>
       </div>
     </>
   );
--- src/component/pages/Portfolio.js
+++ src/component/pages/Portfolio.js
@@ -8,140 +8,153 @@ import Noteimg from "../../images/Notetaker.png";
 import Passwordimg from "../../images/PasswordGenerator.png";
 import Schedulerimg from "../../images/scheduler.png";
 import Weather from "../../images/weather dashboard.png";
+import { FaGithubSquare } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const Portfolio = () => {
   return (
     <>
       <div className="container">
-        <div className="row">
-          <div className="col-lg-6 mb-4">
-            <Card className="text-center">
-              <Card.Body>
-                <Card.Title>
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://codeapedia.herokuapp.com/">Code-a-pedia</a>
-                  </Button>
-                  {"  "}
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://github.com/pjsherpa/Code-a-pedia">
-                      github
-                    </a>
-                  </Button>
-                  <Card.Img src={Codeapediaimg} />
-                </Card.Title>
-                <Card.Text>
-                  This is a knowledge bank for coding terms/definitions/slangs.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+        <IconContext.Provider value={{ color: "black", size: "40px" }}>
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <Card className="text-center hovercard">
+                <Card.Body>
+                  <Card.Title>
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://codeapedia.herokuapp.com/">
+                        Code-a-pedia
+                      </a>
+                    </Button>
+                    {"  "}
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://github.com/pjsherpa/Code-a-pedia">
+                        <FaGithubSquare />
+                      </a>
+                    </Button>
+                    <Card.Img src={Codeapediaimg} />
+                  </Card.Title>
+                  <Card.Text>
+                    This is a knowledge bank for coding
+                    terms/definitions/slangs.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <Card className="text-center hovercard">
+                <Card.Body>
+                  <Card.Title>
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://pjsherpa.github.io/AirSoup/">AirSoup</a>
+                    </Button>
+                    {"  "}
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://github.com/pjsherpa/AirSoup">
+                        {" "}
+                        <FaGithubSquare />
+                      </a>
+                    </Button>
+                    <Card.Img src={AirSoupimg} />
+                  </Card.Title>
+                  <Card.Text>
+                    Plan a trip to eat outside depending on the current air
+                    quality.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
-          <div className="col-lg-6 mb-4">
-            <Card className="text-center">
-              <Card.Body>
-                <Card.Title>
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://pjsherpa.github.io/AirSoup/">AirSoup</a>
-                  </Button>
-                  {"  "}
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://github.com/pjsherpa/AirSoup">github</a>
-                  </Button>
-                  <Card.Img src={AirSoupimg} />
-                </Card.Title>
-                <Card.Text>
-                  Plan a trip to eat outside depending on the current air
-                  quality.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <Card className="text-center hovercard">
+                <Card.Body>
+                  <Card.Title>
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://pjsherpa.github.io/weather-dashboard-v.01/">
+                        Weather
+                      </a>
+                    </Button>
+                    {"  "}
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://github.com/pjsherpa/weather-dashboard-v.01">
+                        <FaGithubSquare />
+                      </a>
+                    </Button>
+                    <Card.Img src={Weather} />
+                  </Card.Title>
+                  <Card.Text>Weather Forecast</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <Card className="text-center hovercard">
+                <Card.Body>
+                  <Card.Title>
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://cryptic-island-27891.herokuapp.com/">
+                        Note Taker
+                      </a>
+                    </Button>
+                    {"  "}
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://github.com/pjsherpa/Note-Taker">
+                        {" "}
+                        <FaGithubSquare />
+                      </a>
+                    </Button>
+                    <Card.Img src={Noteimg} />
+                  </Card.Title>
+                  <Card.Text>Note takers</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 mb-4">
-            <Card className="text-center">
-              <Card.Body>
-                <Card.Title>
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://pjsherpa.github.io/weather-dashboard-v.01/">
-                      Weather
-                    </a>
-                  </Button>
-                  {"  "}
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://github.com/pjsherpa/weather-dashboard-v.01">
-                      github
-                    </a>
-                  </Button>
-                  <Card.Img src={Weather} />
-                </Card.Title>
-                <Card.Text>Weather Forecast</Card.Text>
-              </Card.Body>
-            </Card>
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <Card className="text-center hovercard">
+                <Card.Body>
+                  <Card.Title>
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://pjsherpa.github.io/Work-Day-Scheduler/">
+                        Scheduler
+                      </a>
+                    </Button>
+                    {"  "}
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://github.com/pjsherpa/Work-Day-Scheduler">
+                        <FaGithubSquare />
+                      </a>
+                    </Button>
+                    <Card.Img src={Schedulerimg} />
+                  </Card.Title>
+                  <Card.Text>Work Day Scheduler</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <Card className="text-center hovercard">
+                <Card.Body>
+                  <Card.Title>
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://pjsherpa.github.io/Password-Generator/">
+                        Create Password
+                      </a>
+                    </Button>
+                    {"  "}
+                    <Button className="bg-image hoversm" variant="transparent">
+                      <a href="https://github.com/pjsherpa/Password-Generator">
+                        <FaGithubSquare />
+                      </a>
+                    </Button>
+                    <Card.Img src={Passwordimg} />
+                  </Card.Title>
+                  <Card.Text>Geneate password</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
-          <div className="col-lg-6 mb-4">
-            <Card className="text-center">
-              <Card.Body>
-                <Card.Title>
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://cryptic-island-27891.herokuapp.com/">
-                      Note Taker
-                    </a>
-                  </Button>
-                  {"  "}
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://github.com/pjsherpa/Note-Taker">github</a>
-                  </Button>
-                  <Card.Img src={Noteimg} />
-                </Card.Title>
-                <Card.Text>Note takers</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 mb-4">
-            <Card className="text-center">
-              <Card.Body>
-                <Card.Title>
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://pjsherpa.github.io/Work-Day-Scheduler/">
-                      Scheduler
-                    </a>
-                  </Button>
-                  {"  "}
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://github.com/pjsherpa/Work-Day-Scheduler">
-                      github
-                    </a>
-                  </Button>
-                  <Card.Img src={Schedulerimg} />
-                </Card.Title>
-                <Card.Text>Work Day Scheduler</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="col-lg-6 mb-4">
-            <Card className="text-center">
-              <Card.Body>
-                <Card.Title>
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://pjsherpa.github.io/Password-Generator/">
-                      Create Password
-                    </a>
-                  </Button>
-                  {"  "}
-                  <Button className="bg-image hoversm" variant="transparent">
-                    <a href="https://github.com/pjsherpa/Password-Generator">
-                      github
-                    </a>
-                  </Button>
-                  <Card.Img src={Passwordimg} />
-                </Card.Title>
-                <Card.Text>Geneate password</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
+        </IconContext.Provider>
       </div>
     </>
   );
