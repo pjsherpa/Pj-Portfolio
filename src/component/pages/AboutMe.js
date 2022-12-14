8ae5a60f5f15b58ceaa632584cf1271102963b7e
Phinjock Sherpa <phinjock@gmail.com>
2022-12-14 08:08:41 -0800

    updated about and resume

--- src/component/pages/AboutMe.js
+++ src/component/pages/AboutMe.js
@@ -13,13 +13,13 @@ const AboutMe = () => {
         </div>
         <br></br>
         <p className="text-center">
-          My name is Phinjock Sherpa aka Pj. Having the oppurtunity to work
-          alongside developers in my previous job as an end user has inspired
-          and lead me to study full stack web development. I have completed my
-          full stack web development at UC Berkeley. I have a keen desire to
-          learn new technology and continuing my coding practices through other
-          means of online studies currently. My aim is to become a Software
-          Engineer.
+          My name is Phinjock Sherpa aka Pj. I have completed my course in full
+          stack webdevelopment in UC Berkeley. Having the oppurtunity to work
+          alongside developers in my previous job as an end user had inspired
+          and lead me to study full stack web development. I have a keen desire
+          to learn new technology and continuing my coding practices through
+          other means of online studies. My aim is to become a Software Engineer
+          and I will be a valuable member to any team I join.
         </p>
         <p className="text-center">
           Personal side note:I'm adventurous and love to travel.{" "}
