9d4e445d24a1b10289a3522dd72f0e3290d5ce78
Phinjock Sherpa <phinjock@gmail.com>
2023-09-10 17:54:49 -0700

    updated about me

--- src/component/pages/AboutMe.js
+++ src/component/pages/AboutMe.js
@@ -13,19 +13,36 @@ const AboutMe = () => {
         </div>
         <br></br>
         <p className="text-center">
-          My name is Phinjock Sherpa preferred name Pj. I have completed my
-          course in full stack webdevelopment in UC Berkeley. Having the
-          oppurtunity to work alongside developers in my previous job as an end
-          user had inspired and lead me to study full stack web development. I
-          have a keen desire to learn new technology and continuing my coding
-          practices through other means of online studies. My aim is to become a
-          Software Engineer and I will be a valuable member to any team I join.
+          Greetings, I am Phinjock Sherpa, but I prefer to go by Pj. I recently
+          completed a rigorous full-stack web development program at UC
+          Berkeley, and I am excited to showcase my journey and aspirations as a
+          React Developer. Educational Background My journey into web
+          development was inspired by my previous role as an end user, where I
+          had the privilege of working alongside skilled developers. This
+          experience ignited my passion for coding and led me to pursue a
+          comprehensive education in full-stack web development at UC Berkeley.
+          The program equipped me with a strong foundation in web technologies,
+          including front-end and back-end development, database management, and
+          more. Passion for Learning One of my defining traits is my unwavering
+          desire to learn and adapt to emerging technologies. I believe that the
+          tech industry is ever-evolving, and staying current is essential. To
+          this end, I have consistently sought out additional online courses and
+          resources to enhance my skills. My dedication to continuous
+          improvement drives me to stay at the forefront of industry trends.
+          Career Aspirations My ultimate goal is to become a Software Engineer.
+          I am confident that my dedication, problem-solving abilities, and
+          collaborative mindset make me a valuable asset to any development
+          team. I am eager to contribute to innovative projects, tackle complex
+          challenges, and expand my knowledge in the field. Personal Interests
+          Beyond my professional pursuits, I am an adventurous individual who
+          loves to travel and explore new places. This passion for exploration
+          mirrors the dynamic and ever-changing nature of the tech industry, and
+          I believe it adds a unique dimension to my perspective as a developer.
+          Thank you for visiting my portfolio. I invite you to explore my work
+          and accomplishments in more detail. I look forward to the opportunity
+          to discuss how I can contribute to your team's success. Best regards,
+          Phinjock Sherpa (Pj)
         </p>
-        <p className="text-center">
-          Personal side note:I'm adventurous and love to travel.{" "}
-        </p>
-
-        <p className="text-center">Thank you for visiting my portfolio.</p>
       </div>
     </>
   );
